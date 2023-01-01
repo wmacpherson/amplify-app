ada6a8f76066ea61519620946cad15b9642b21a9
wmacpherson <117464801+wmacpherson@users.noreply.github.com>
2023-01-01 17:54:00 +0000

    fixed routing

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,37 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      score
+      firm
+      position
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        score
+        firm
+        position
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
 export const getPrediction = /* GraphQL */ `
   query GetPrediction($id: ID!) {
     getPrediction(id: $id) {
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,6 +1,42 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser($filter: ModelSubscriptionUserFilterInput) {
+    onCreateUser(filter: $filter) {
+      id
+      score
+      firm
+      position
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser($filter: ModelSubscriptionUserFilterInput) {
+    onUpdateUser(filter: $filter) {
+      id
+      score
+      firm
+      position
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser($filter: ModelSubscriptionUserFilterInput) {
+    onDeleteUser(filter: $filter) {
+      id
+      score
+      firm
+      position
+      createdAt
+      updatedAt
+    }
+  }
+`;
 export const onCreatePrediction = /* GraphQL */ `
   subscription OnCreatePrediction(
     $filter: ModelSubscriptionPredictionFilterInput
